fc7b5e142ad7e4c5361beb44101b5ef65c2c3676
Двуликий Янус <twofacedjanus@MacBook-Pro-Dvulikij.local>
2024-01-11 19:25:00 +0500

    pointer

--- scripts/test_scene.gd
+++ scripts/test_scene.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+var pointer = load("res://assets/Tiny Swords (Update 010)/UI/Pointers/pointer32.png");
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	Input.set_custom_mouse_cursor(pointer);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
