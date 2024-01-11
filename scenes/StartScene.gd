d22b38a7aeb07291e2c96bee8344cdb78f1acb1f
Двуликий Янус <twofacedjanus@MacBook-Pro-Dvulikij.local>
2024-01-11 21:42:37 +0500

    new way

---
+++ scenes/StartScene.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var pointer = load("res://assets/Tiny Swords (Update 010)/UI/Pointers/pointer32.png");
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/polygon.tscn");
+
+func _on_button_2_pressed():
+	get_tree().quit();
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_custom_mouse_cursor(pointer);
+
