4b5b1dffe87b3c08d3b052e4c03b500da1d75599
Двуликий Янус <twofacedjanus@MacBook-Pro-Dvulikij.local>
2024-01-11 22:11:55 +0500

    rocks on water

--- scenes/StartScene.gd
+++ scenes/StartScene.gd
@@ -3,6 +3,7 @@ extends Node2D
 var pointer = load("res://assets/Tiny Swords (Update 010)/UI/Pointers/pointer32.png");
 
 
+
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/polygon.tscn");
 
@@ -12,4 +13,4 @@ func _on_button_2_pressed():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(pointer);
-
+	
