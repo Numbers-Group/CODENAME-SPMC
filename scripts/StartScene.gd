2e386df9c5d07afaa37aea44e7ef3ac5ae8eccc7
Двуликий Янус <twofacedjanus@MacBook-Pro-Dvulikij.local>
2024-01-12 13:30:12 +0500

    new button in start screen

--- scripts/StartScene.gd
+++ scripts/StartScene.gd
@@ -5,7 +5,7 @@ var pointer = load("res://assets/Tiny Swords (Update 010)/UI/Pointers/pointer32.
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/polygon.tscn");
+	get_tree().change_scene_to_file("res://scenes/levels/polygon.tscn");
 
 func _on_button_2_pressed():
 	get_tree().quit();
@@ -13,4 +13,9 @@ func _on_button_2_pressed():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(pointer);
+	
+
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/SimplePolygon.tscn");
 	
